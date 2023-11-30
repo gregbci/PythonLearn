cdbd4d492536b9054e0cb089d36f4e920063e336
Greg Wilding <gregory.wilding@ucalgary.ca>
2023-11-30 12:52:36 -0700

    Adding properties to ABC also

--- mymodule/abstract.py
+++ mymodule/abstract.py
@@ -5,15 +5,24 @@ class Dog(ABC):
    def speak(self):
       pass
 
+   @property
+   @abstractmethod
+   def breed(self) -> str:
+      pass
+
 
 class Poodle(Dog):
-   def __init__(self):
-      self.breed = "poodle"
 
    def speak(self):
       print(self.breed + " woof")
 
+   @property
+   def breed(self) -> str:
+      return "poodle"
 
-dog = Poodle()
-dog.speak()
+dog = Poodle()     #  <-- can't instantiate if Dog isn't implemented completely
+dog.speak()        
+print(dog.breed)   #  <-- this behaves just like a variable
+
+# dog.breed = "cabbage"  #  <-- this fails because property is readonly
 
