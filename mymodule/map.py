eb952a99f9c7c5826d2ca572e91d3dcaf8882944
Greg Wilding <gregory.wilding@ucalgary.ca>
2023-11-30 20:00:46 -0700

    Messing with map

--- mymodule/map.py
+++ mymodule/map.py
@@ -1,3 +1,10 @@
 names = ["Foo", "Bar", "Baz"]
+lowercase = list(map(str.lower, names))
+print(lowercase)
 
-print(list(map(str.lower, names)))
+data = [["Foo", 1], ["Bar", 2], ["Baz", 3]]
+
+names = [x[0] for x in data]
+nums = [x[1] for x in data]
+print(names)
+print(nums)
