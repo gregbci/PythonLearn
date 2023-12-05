3fe29ed1a9db02bf54fce6338445a4dd0481aa07
Greg Wilding <gregory.wilding@ucalgary.ca>
2023-12-05 10:23:33 -0700

    Mucking with map and list comprehension

--- mymodule/map.py
+++ mymodule/map.py
@@ -1,10 +1,21 @@
-names = ["Foo", "Bar", "Baz"]
-lowercase = list(map(str.lower, names))
-print(lowercase)
-
 data = [["Foo", 1], ["Bar", 2], ["Baz", 3]]
 
 names = [x[0] for x in data]
 nums = [x[1] for x in data]
 print(names)
 print(nums)
+
+
+names = ["Foo", "Bar", "Baz"]
+lowercase = list(map(str.lower, names))
+print(lowercase)
+
+
+yelling = list(map(lambda name: name + "!", names))
+print(yelling)
+
+
+replaced = [name.replace("Bar", "Quux") for name in names]
+print(replaced)
+
+
